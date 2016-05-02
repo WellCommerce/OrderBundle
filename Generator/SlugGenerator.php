165cd4c24aecdbb8d51a88b8b0a59e98b254811c
Adam Piotrowski <adam@wellcommerce.org>
2016-05-02 14:34:36 +0200

    Major refactorings of managers, factories, repositories

--- Generator/SlugGenerator.php
+++ Generator/SlugGenerator.php
@@ -1,52 +1,55 @@
 <?php
 /*
  * WellCommerce Open-Source E-Commerce Platform
- *
+ * 
  * This file is part of the WellCommerce package.
  *
  * (c) Adam Piotrowski <adam@wellcommerce.org>
- *
+ * 
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\RoutingBundle\Manager;
+namespace WellCommerce\Bundle\RoutingBundle\Generator;
 
 use WellCommerce\Bundle\CoreBundle\Helper\Sluggable;
-use WellCommerce\Bundle\CoreBundle\Manager\AbstractManager;
-use WellCommerce\Bundle\CoreBundle\Manager\Admin\AbstractAdminManager;
+use WellCommerce\Bundle\CoreBundle\Manager\ManagerInterface;
 use WellCommerce\Bundle\RoutingBundle\Entity\RouteInterface;
 
 /**
- * Class RoutingManager
+ * Class SlugGenerator
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class RoutingManager extends AbstractManager
+final class SlugGenerator implements SlugGeneratorInterface
 {
     /**
-     * Generates and validates uniqueness of slug
-     *
-     * @param string   $name   Passed value to generate slug
-     * @param int|null $id     Entity id
-     * @param string   $locale Field locale
-     * @param array    $values Other sluggable field values
-     * @param int      $iteration
-     *
-     * @return string
+     * @var ManagerInterface
      */
-    public function generateSlug($name, $id, $locale, $values, $iteration = 0)
+    private $manager;
+    
+    /**
+     * SlugGenerator constructor.
+     *
+     * @param ManagerInterface $manager
+     */
+    public function __construct(ManagerInterface $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function generate(string $name, $id, string $locale, $values, int $iteration = 0) : string
     {
         $slug           = Sluggable::makeSlug($name);
         $existsInValues = in_array($slug, (array)$values);
-
+        
         // if slug is the same as other values, try to add locale part
         if ($existsInValues) {
             $slug = sprintf('%s-%s', $slug, $locale);
         }
-
-        $route = $this->getRepository()->findOneBy(['path' => $slug]);
-
+        
+        $route = $this->manager->getRepository()->findOneBy(['path' => $slug]);
+        
         if (null !== $route) {
             // if passed identifier and locale are same as in route, assume we can change the slug directly
             if ($this->hasRouteSameLocaleAndId($route, $locale, $id)) {
@@ -54,14 +57,14 @@ class RoutingManager extends AbstractManager
             } else {
                 $iteration++;
                 $slug = $this->makeSlugIterated($slug, $iteration);
-
-                return $this->generateSlug($slug, $id, $locale, $values, $iteration);
+                
+                return $this->generate($slug, $id, $locale, $values, $iteration);
             }
         }
-
+        
         return $slug;
     }
-
+    
     /**
      * Checks passed identifier and locale against those in route
      *
@@ -71,11 +74,11 @@ class RoutingManager extends AbstractManager
      *
      * @return bool
      */
-    protected function hasRouteSameLocaleAndId(RouteInterface $route, $locale, $id)
+    private function hasRouteSameLocaleAndId(RouteInterface $route, string $locale, $id) : bool
     {
         return ($route->getIdentifier()->getId() === $id && $route->getLocale() === $locale);
     }
-
+    
     /**
      * Makes original slug iterated
      *
@@ -84,7 +87,7 @@ class RoutingManager extends AbstractManager
      *
      * @return string
      */
-    protected function makeSlugIterated($slug, $iteration)
+    private function makeSlugIterated($slug, $iteration)
     {
         return sprintf('%s%s%s', $slug, Sluggable::SLUG_DELIMITER, $iteration);
     }
