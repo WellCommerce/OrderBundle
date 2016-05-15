0e4b0ff4dd931c236e6f13b9a09898728fd5358c
Adam Piotrowski <adam@wellcommerce.org>
2016-05-15 18:52:05 +0200

    Fixed manager tests

---
+++ Tests/Manager/CouponManagerTest.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CouponBundle\Tests\Manager;
+
+use WellCommerce\Bundle\CoreBundle\Test\Manager\AbstractManagerTestCase;
+
+/**
+ * Class CouponManagerTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class CouponManagerTest extends AbstractManagerTestCase
+{
+    protected function get()
+    {
+        return $this->container->get('coupon.manager');
+    }
+}
