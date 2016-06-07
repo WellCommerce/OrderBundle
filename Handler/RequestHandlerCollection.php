d119c447c882975978ea4e4302851e0de61a7e54
Adam Piotrowski <adam@wellcommerce.org>
2016-06-07 13:52:52 +0200

    Changed configuration files and configured extensions

--- Handler/RequestHandlerCollection.php
+++ Handler/RequestHandlerCollection.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\ApiBundle\Request;
+namespace WellCommerce\Bundle\ApiBundle\Handler;
 
 use WellCommerce\Bundle\ApiBundle\Exception\NonUniqueRequestHandlerException;
 use WellCommerce\Bundle\ApiBundle\Exception\RequestHandlerNotFoundException;
