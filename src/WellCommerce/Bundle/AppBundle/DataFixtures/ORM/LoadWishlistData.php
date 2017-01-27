d71d4b893c54537423c92dc24d75d64d0ca26189
Adam Piotrowski <adam@wellcommerce.org>
2017-01-27 01:00:32 +0100

    Moved all fixtures to AppBundle

---
+++ src/WellCommerce/Bundle/AppBundle/DataFixtures/ORM/LoadWishlistData.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use WellCommerce\Bundle\ClientBundle\Entity\Client;
+use WellCommerce\Bundle\ClientBundle\Entity\ClientBillingAddress;
+use WellCommerce\Bundle\ClientBundle\Entity\ClientInterface;
+use WellCommerce\Bundle\ClientBundle\Entity\ClientShippingAddress;
+use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\LayoutBundle\Entity\LayoutBox;
+use WellCommerce\Bundle\LayoutBundle\Entity\LayoutBoxTranslation;
+
+/**
+ * Class LoadWishlistData
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LoadWishlistData extends AbstractDataFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        if (!$this->isEnabled()) {
+            return;
+        }
+        
+        $this->createLayoutBoxes($manager, [
+            'wishlist' => [
+                'type' => 'Wishlist',
+                'name' => 'Wishlist',
+            ],
+        ]);
+        
+        $manager->flush();
+    }
+}
