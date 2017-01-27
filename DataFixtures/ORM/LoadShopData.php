d66308b57452ecc5b979abcc124bbf5c864c09ad
Adam Piotrowski <adam@wellcommerce.org>
2017-01-27 01:00:32 +0100

    Moved all fixtures to AppBundle

---
+++ DataFixtures/ORM/LoadShopData.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use WellCommerce\Bundle\AppBundle\Entity\MailerConfiguration;
+use WellCommerce\Bundle\CoreBundle\DataFixtures\AbstractDataFixture;
+use WellCommerce\Bundle\CurrencyBundle\DataFixtures\ORM\LoadCurrencyData;
+use WellCommerce\Bundle\ShopBundle\Entity\Shop;
+
+/**
+ * Class LoadShopData
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class LoadShopData extends AbstractDataFixture
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        if (!$this->isEnabled()) {
+            return;
+        }
+        
+        /**
+         * @var $theme       \WellCommerce\Bundle\ThemeBundle\Entity\ThemeInterface
+         * @var $company     \WellCommerce\Bundle\CompanyBundle\Entity\CompanyInterface
+         * @var $orderStatus \WellCommerce\Bundle\OrderBundle\Entity\OrderStatusInterface
+         */
+        $theme    = $this->getReference('theme');
+        $company  = $this->getReference('company');
+        $currency = $this->randomizeSamples('currency', LoadCurrencyData::$samples);
+        
+        $shop = new Shop();
+        $shop->setName('WellCommerce');
+        $shop->setCompany($company);
+        $shop->setTheme($theme);
+        $shop->setUrl($this->container->getParameter('fallback_hostname'));
+        $shop->setDefaultCountry('US');
+        $shop->setDefaultCurrency($currency->getCode());
+        $shop->setClientGroup($this->getReference('client_group'));
+        
+        $mailerConfiguration = new MailerConfiguration();
+        $mailerConfiguration->setFrom($this->container->getParameter('mailer_from'));
+        $mailerConfiguration->setHost($this->container->getParameter('mailer_host'));
+        $mailerConfiguration->setPort($this->container->getParameter('mailer_port'));
+        $mailerConfiguration->setUser($this->container->getParameter('mailer_user'));
+        $mailerConfiguration->setPass($this->container->getParameter('mailer_password'));
+        $mailerConfiguration->setBcc($this->container->getParameter('mailer_from'));
+        
+        $shop->setMailerConfiguration($mailerConfiguration);
+        
+        $manager->persist($shop);
+        $manager->flush();
+        
+        $this->get('shop.storage')->setCurrentShop($shop);
+        $this->setReference('shop', $shop);
+    }
+}
