30584e3f1d315041ba8b7d33ecbb8ed7e83ce60b
Adam Piotrowski <adam@wellcommerce.org>
2017-01-29 15:12:03 +0100

    Added Enableable, Sortable, Identifiable and replaced mappings with loadClassMetadata events

--- Entity/Coupon.php
+++ Entity/Coupon.php
@@ -15,7 +15,7 @@ namespace WellCommerce\Bundle\CouponBundle\Entity;
 use Knp\DoctrineBehaviors\Model\Blameable\Blameable;
 use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
 use Knp\DoctrineBehaviors\Model\Translatable\Translatable;
-use WellCommerce\Bundle\DoctrineBundle\Entity\IdentifiableTrait;
+use WellCommerce\Bundle\DoctrineBundle\Behaviours\Identifiable;
 
 /**
  * Class Coupon
@@ -24,7 +24,7 @@ use WellCommerce\Bundle\DoctrineBundle\Entity\IdentifiableTrait;
  */
 class Coupon implements CouponInterface
 {
-    use IdentifiableTrait;
+    use Identifiable;
     use Translatable;
     use Timestampable;
     use Blameable;
