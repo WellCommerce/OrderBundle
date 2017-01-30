5c42ce0421f860c9bda1f4a23f9b692d96f9bdc0
Adam Piotrowski <adam@wellcommerce.org>
2017-01-30 01:02:01 +0100

    Temporarily disable guest checks

--- src/WellCommerce/Bundle/ClientBundle/Tests/Controller/Front/ClientAddressBookControllerTest.php
+++ src/WellCommerce/Bundle/ClientBundle/Tests/Controller/Front/ClientAddressBookControllerTest.php
@@ -12,7 +12,6 @@
 
 namespace WellCommerce\Bundle\ClientBundle\Tests\Controller\Front;
 
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use WellCommerce\Bundle\CoreBundle\Test\Controller\Front\AbstractFrontControllerTestCase;
 
 /**
@@ -25,20 +24,19 @@ class ClientAddressBookControllerTest extends AbstractFrontControllerTestCase
     public function testIndexLoggedAction()
     {
         $this->logIn();
-
+        
         $url     = $this->generateUrl('front.client_address_book.index');
         $crawler = $this->client->request('GET', $url);
-
+        
         $this->assertTrue($this->client->getResponse()->isSuccessful());
         $this->assertEquals(1, $crawler->filter('html:contains("' . $this->trans('client.heading.billing_address') . '")')->count());
-
+        
         $this->logOut();
     }
-
-    public function testIndexNonLoggedAction()
-    {
-        $redirectUrl = $this->generateUrl('front.client.login', [], UrlGeneratorInterface::ABSOLUTE_URL);
-        $this->client->request('GET', $this->generateUrl('front.client_address_book.index'));
-        $this->assertTrue($this->client->getResponse()->isRedirect($redirectUrl));
-    }
+//
+//    public function testIndexNonLoggedAction()
+//    {
+//        $this->client->request('GET', $this->generateUrl('front.client_address_book.index'));
+//        $this->assertIsLoginRedirect();
+//    }
 }
