8ee1e5cb1e58fe7150cb007648f0e510ab4ab49f
Adam Piotrowski <adam@wellcommerce.org>
2017-01-30 13:45:03 +0100

    FacebookClientManager

---
+++ src/WellCommerce/Bundle/OAuthBundle/Manager/FacebookClientManager.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\OAuthBundle\Manager;
+
+use League\OAuth2\Client\Provider\FacebookUser;
+use Symfony\Component\Security\Core\User\UserInterface;
+use WellCommerce\Bundle\ClientBundle\Entity\Client;
+use WellCommerce\Bundle\CoreBundle\Helper\Helper;
+use WellCommerce\Bundle\CoreBundle\Manager\AbstractManager;
+
+/**
+ * Class FacebookClientManager
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class FacebookClientManager extends AbstractManager
+{
+    public function getClient(FacebookUser $facebookUser): UserInterface
+    {
+        $email = $facebookUser->getEmail();
+        
+        $client = $this->repository->findOneBy([
+            'clientDetails.username' => $email,
+        ]);
+        
+        if (!$client instanceof Client) {
+            $client = $this->autoRegisterFacebookUser($facebookUser);
+        }
+        
+        return $client;
+    }
+    
+    private function autoRegisterFacebookUser(FacebookUser $facebookUser): UserInterface
+    {
+        $firstName = $facebookUser->getFirstName();
+        $lastName  = $facebookUser->getLastName();
+        $email     = $facebookUser->getEmail();
+        
+        /** @var $client Client */
+        $client = $this->initResource();
+        $client->getClientDetails()->setUsername($email);
+        $client->getClientDetails()->setPassword(Helper::generateRandomPassword());
+        
+        $client->getContactDetails()->setEmail($email);
+        $client->getContactDetails()->setFirstName($firstName);
+        $client->getContactDetails()->setLastName($lastName);
+        $client->getContactDetails()->setPhone(' ');
+        $client->getContactDetails()->setSecondaryPhone(' ');
+        
+        $this->createResource($client);
+        
+        return $client;
+    }
+}
