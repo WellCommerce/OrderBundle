f23332b265ab485db51751b07ab7c166bdd74f09
Adam Piotrowski <adam@wellcommerce.org>
2017-02-04 22:55:01 +0100

    Moved UnitBundle to CatalogBundle

---
+++ Entity/UnitTranslation.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\CatalogBundle\Entity;
+
+use Knp\DoctrineBehaviors\Model\Translatable\Translation;
+use WellCommerce\Bundle\LocaleBundle\Entity\LocaleAwareInterface;
+
+/**
+ * UnitTranslation
+ */
+class UnitTranslation implements LocaleAwareInterface
+{
+    use Translation;
+    
+    protected $name = '';
+    
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+}
