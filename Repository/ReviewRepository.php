647b1919484ba0e7521b3c64bdb887438c3b18e4
Adam Piotrowski <adam@wellcommerce.org>
2017-02-05 02:01:59 +0100

    Merged DoctrineBundle into CoreBundle

--- Repository/ReviewRepository.php
+++ Repository/ReviewRepository.php
@@ -13,7 +13,7 @@ namespace WellCommerce\Bundle\ReviewBundle\Repository;
 
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
-use WellCommerce\Bundle\DoctrineBundle\Repository\EntityRepository;
+use WellCommerce\Bundle\CoreBundle\Repository\EntityRepository;
 use WellCommerce\Bundle\CatalogBundle\Entity\Product;
 
 /**
