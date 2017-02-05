4ba66841ec04fbb4e5ac78dadd50e0f7a56ccd11
Adam Piotrowski <adam@wellcommerce.org>
2017-02-05 12:59:53 +0100

    Moved Dictionary bundles to AppBundle

---
+++ Entity/DictionaryTranslation.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ *
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\AppBundle\Entity;
+
+use Knp\DoctrineBehaviors\Model\Translatable\Translation;
+use WellCommerce\Bundle\AppBundle\Entity\LocaleAwareInterface;
+
+/**
+ * Class DictionaryTranslation
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class DictionaryTranslation implements LocaleAwareInterface
+{
+    use Translation;
+    
+    protected $value = '';
+    
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+    
+    public function setValue(string $value)
+    {
+        $this->value = $value;
+    }
+}
