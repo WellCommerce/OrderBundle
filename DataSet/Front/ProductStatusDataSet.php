14e688ce2c45fb34f9985a3d95cd05ef3dbee0d1
Adam Piotrowski <adam@wellcommerce.org>
2017-02-05 02:12:05 +0100

    Moved abstract classes to core bundle. Moved countrybundle to appbundle

--- DataSet/Front/ProductStatusDataSet.php
+++ DataSet/Front/ProductStatusDataSet.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\CatalogBundle\DataSet\Front;
 
 use Doctrine\ORM\QueryBuilder;
-use WellCommerce\Bundle\AppBundle\DataSet\AbstractDataSet;
+use WellCommerce\Bundle\CoreBundle\DataSet\AbstractDataSet;
 use WellCommerce\Bundle\CatalogBundle\Entity\ProductStatus;
 use WellCommerce\Bundle\CatalogBundle\Entity\ProductStatusTranslation;
 use WellCommerce\Component\DataSet\Cache\CacheOptions;
