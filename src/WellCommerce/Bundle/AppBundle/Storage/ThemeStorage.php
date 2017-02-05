35165647a37525bf49e9cccb909da602652157da
Adam Piotrowski <adam@wellcommerce.org>
2017-02-05 14:54:05 +0100

    Moved ThemeContext to storage namespace

--- src/WellCommerce/Bundle/AppBundle/Storage/ThemeStorage.php
+++ src/WellCommerce/Bundle/AppBundle/Storage/ThemeStorage.php
@@ -10,16 +10,16 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\AppBundle\Context\Front;
+namespace WellCommerce\Bundle\AppBundle\Storage;
 
 use WellCommerce\Bundle\AppBundle\Entity\Theme;
 
 /**
- * Class ThemeContext
+ * Class ThemeStorage
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class ThemeContext implements ThemeContextInterface
+class ThemeStorage implements ThemeStorageInterface
 {
     /**
      * @var Theme
