0b55de22f2a3506d5844bb98cf22c8557d4dca79
Adam Piotrowski <adam@wellcommerce.org>
2017-02-08 01:44:27 +0100

    Removed deprecated generateUUIDv5

--- Helper/Helper.php
+++ Helper/Helper.php
@@ -101,18 +101,4 @@ class Helper
         
         return $result;
     }
-
-    public static function generateUUIDv5() : string
-    {
-        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            random_int(0, 0xffff),
-            random_int(0, 0xffff),
-            random_int(0, 0xffff),
-            random_int(0, 0x0fff) | 0x4000,
-            random_int(0, 0x3fff) | 0x8000,
-            random_int(0, 0xffff),
-            random_int(0, 0xffff),
-            random_int(0, 0xffff)
-        );
-    }
 }
