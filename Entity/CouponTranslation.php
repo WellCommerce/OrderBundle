20c48f8e94e17ee358ebaaea2e707d74cdca6c53
Adam Piotrowski <adam@wellcommerce.org>
2017-02-12 15:13:11 +0100

    Finished DoctrineBundle

--- Entity/CouponTranslation.php
+++ Entity/CouponTranslation.php
@@ -13,7 +13,7 @@
 namespace WellCommerce\Bundle\CouponBundle\Entity;
 
 use Knp\DoctrineBehaviors\Model\Translatable\Translation;
-use WellCommerce\Bundle\CoreBundle\Entity\AbstractTranslation;
+use WellCommerce\Bundle\DoctrineBundle\Entity\AbstractTranslation;
 
 /**
  * Class CouponTranslation
