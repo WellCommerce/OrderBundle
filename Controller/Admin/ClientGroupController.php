080c4304213f66e44b08282f444acdb7d445f7d0
Adam Piotrowski <adam@wellcommerce.org>
2017-02-14 00:14:52 +0100

    Fixed ClientGroupController and tests

--- Controller/Admin/ClientGroupController.php
+++ Controller/Admin/ClientGroupController.php
@@ -12,52 +12,13 @@
 
 namespace WellCommerce\Bundle\AppBundle\Controller\Admin;
 
-use Symfony\Component\HttpFoundation\Response;
-use WellCommerce\Bundle\AppBundle\DataGrid\ClientGroupDataGrid;
-use WellCommerce\Bundle\AppBundle\Form\Admin\ClientGroupFormBuilder;
-use WellCommerce\Bundle\AppBundle\Manager\ClientGroupManager;
-use WellCommerce\Bundle\CoreBundle\Controller\ControllerInterface;
+use WellCommerce\Bundle\CoreBundle\Controller\Admin\AbstractAdminController;
 
 /**
  * Class ClientGroupController
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class ClientGroupController implements ControllerInterface
+class ClientGroupController extends AbstractAdminController
 {
-    private $manager;
-    private $formBuilder;
-    private $dataGrid;
-    
-    public function __construct(ClientGroupManager $manager, ClientGroupFormBuilder $formBuilder, ClientGroupDataGrid $dataGrid)
-    {
-        $this->manager     = $manager;
-        $this->formBuilder = $formBuilder;
-        $this->dataGrid    = $dataGrid;
-    }
-    
-    public function indexAction(): Response
-    {
-        return $this->displayTemplate('index', [
-            'datagrid' => $this->dataGrid,
-        ]);
-    }
-    
-    public function addAction(): Response
-    {
-        $resource = $this->manager->initResource();
-        $form     = $this->formBuilder->createForm(['name' => 'client_group'], $resource);
-        
-        if ($form->handleRequest()->isSubmitted()) {
-            if ($form->isValid()) {
-                $this->getManager()->createResource($resource);
-            }
-            
-            return $this->createFormDefaultJsonResponse($form);
-        }
-        
-        return $this->displayTemplate('add', [
-            'form' => $form,
-        ]);
-    }
 }
