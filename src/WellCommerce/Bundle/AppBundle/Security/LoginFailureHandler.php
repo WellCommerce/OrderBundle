54a36a6de2dea4a2db976f5a8b281767ca068dfc
Adam Piotrowski <adam@wellcommerce.org>
2017-02-15 00:46:19 +0100

    Moved LoginFailureHandler to AppBundle

--- src/WellCommerce/Bundle/AppBundle/Security/LoginFailureHandler.php
+++ src/WellCommerce/Bundle/AppBundle/Security/LoginFailureHandler.php
@@ -10,7 +10,7 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\CoreBundle\Security;
+namespace WellCommerce\Bundle\AppBundle\Security;
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
