36f0620a39b46e0b7186776cd9608125dc2dd19c
Adam Piotrowski <adam@wellcommerce.org>
2017-02-16 01:12:46 +0100

    Introduced DataGridBundle

--- src/WellCommerce/Bundle/DataGridBundle/DataGrid/AbstractDataGrid.php
+++ src/WellCommerce/Bundle/DataGridBundle/DataGrid/AbstractDataGrid.php
@@ -9,7 +9,7 @@
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  */
-namespace WellCommerce\Bundle\CoreBundle\DataGrid;
+namespace WellCommerce\Bundle\DataGridBundle\DataGrid;
 
 use WellCommerce\Component\DataGrid\Column\ColumnCollection;
 use WellCommerce\Component\DataGrid\Configuration\EventHandler\ClickRowEventHandler;
