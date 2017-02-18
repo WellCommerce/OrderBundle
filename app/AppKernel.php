4b00a3232e316e6be57677177c5122b63e87c333
David Maicher <mail@dmaicher.de>
2017-02-18 13:26:42 +0100

    use dama/doctrine-test-bundle to improve test performance (#139)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -62,6 +62,10 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            if ($this->getEnvironment() === 'test') {
+                $bundles[] = new \DAMA\DoctrineTestBundle\DAMADoctrineTestBundle();
+            }
         }
         
         return $bundles;
