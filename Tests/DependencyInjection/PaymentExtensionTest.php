aa89606f28d17c91cce16c2697d3a683ea8ec5f0
Adam Piotrowski <adam@wellcommerce.org>
2017-02-26 00:27:47 +0100

    Added new tests

--- Tests/DependencyInjection/PaymentExtensionTest.php
+++ Tests/DependencyInjection/PaymentExtensionTest.php
@@ -24,7 +24,7 @@ class PaymentMethodExtensionTest extends AbstractExtensionTestCase
     /**
      * @return array
      */
-    public function getRequiredServices()
+    public function getRequiredServices(): array
     {
         return [
             'services' => [
--- Tests/DependencyInjection/ShippingExtensionTest.php
+++ Tests/DependencyInjection/ShippingExtensionTest.php
@@ -24,7 +24,7 @@ class ShippingMethodExtensionTest extends AbstractExtensionTestCase
     /**
      * @return array
      */
-    public function getRequiredServices()
+    public function getRequiredServices(): array
     {
         return [
             'services' => [
